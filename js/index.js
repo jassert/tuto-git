839e770266c2f639da356c20232e51b2ac4a06af
jasser <jasser112011@gmail.com>
2021-11-14 09:42:58 +0100

    test github

---
+++ js/index.js
@@ -0,0 +1,55 @@
+// var student = "jasser"
+
+// function write_student() {
+// student= "sami";
+// console.log(student);
+// }
+
+// function read_student() {
+// console.log("student is :" , student);
+// }
+// write_student();
+// read_student();
+
+// let student ="jasser";
+//  let isstudent = student == "sami"
+//  console.log(isstudent);
+//  console.log(typeof isstudent);
+
+// let student = {
+//     name: "jasser",
+//     age : 23,
+//     language :"arabe",
+//     wheight : "80 kg",
+
+// };
+// let jasser_name = student.name
+//   console.log(student.name);
+//   console.log(typeof student);
+// function sum (a, b)
+
+//     function myCalculator(a, b) {
+//         let sum = a + b;
+//         return sum;
+//       }
+
+//       let result = myCalculator(1, 2);
+// console.log(result)
+
+// function produit (a,b){
+//     return a * b ;
+// }
+// // const produit = (a,b) => a * b ;
+// let result_produit =produit (5,3)
+// console.log(result_produit) ;
+
+function comparer(a, b) {
+  if (a > b) {
+    console.log(a, "superieur à", b);
+  } else if (a < b) {
+    console.log(a, "inferieur", b);
+  } else {
+    console.log(a, "egale à", b);
+  }
+}
+comparer(9, 9);
